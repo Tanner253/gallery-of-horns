61309ae0f656140fabda019d6031b626d46414ee
Tanner253 <PercivalTanner@gmail.com>
2022-02-14 15:03:35 -0800

    adds components for main header and footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <>
+            <h3>&copy;copyright</h3>
+            <p>Tanner Percival</p>
+             </>
+        );
+    }
+}
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Main extends React.Component{
+    render(){
+        return(
+            <p>
+                this is my main p element
+            </p>
+        );
+    }
+}
+export default Main;
