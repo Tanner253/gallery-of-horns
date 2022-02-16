caaab4107c54d7ccc5622c3a254f094eca91c1c4
Tanner253 <PercivalTanner@gmail.com>
2022-02-15 16:15:32 -0800

    finishes lab

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React from "react";
+import "./HornedBeast.css";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
-class HornedBeast extends React.Component{
-    render(){
-        return(
-            <>
-            <h2>{this.props.title}</h2>
-            <img src={this.props.imageURL} alt = "monster" className = "beast-image" title = {this.props.title}/>
-            <p>{this.props.description}</p>
-            </>
-        );
-    }
+class HornedBeast extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = {
+    favorites: 0,
+    };
 }
-export default HornedBeast; 
+handleFavorites = () => {
+    this.setState({
+    favorites: this.state.favorites + 1,
+    });
+};
+render() {
+    return (
+    <article>
+        
+            <Card style={{ width: '18rem' }}>
+                <Card.Img
+                onClick={this.handleFavorites}
+                variant="top"
+                src={this.props.image_url}
+                alt={this.props.description}
+                />
+                <Card.Body>
+                    <Card.Title>{this.props.Title}</Card.Title>
+                    <Card.Text>{this.props.description}</Card.Text>
+                    <Card.Text>💖: {this.state.favorites}</Card.Text>
+                </Card.Body>
+            </Card>
+        
+    </article>
+    );
+}
 
+}
+export default HornedBeast;
