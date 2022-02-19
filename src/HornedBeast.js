cea04ef0a918bd0db60ac674100154419125b9a9
Tanner253 <PercivalTanner@gmail.com>
2022-02-19 13:59:31 -0800

    things not catch on fire woot

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./HornedBeast.css";
 import Card from "react-bootstrap/Card";
 
-
 class HornedBeast extends React.Component {
 constructor(props) {
     super(props);
@@ -18,28 +17,25 @@ handleFavorites = () => {
 handleUserClick = () => {
     this.handleFavorites();
     this.props.handleOpenModal(this.props.beast);
-}
+};
 render() {
     return (
     <article className="text-center">
-        
-            <Card style={{ width: '18rem' }}>
-                <Card.Img
-                onClick={this.handleUserClick}
-                variant="top"
-                src={this.props.beast.image_url}
-                alt={this.props.beast.description}
-                />
-                <Card.Body>
-                    <Card.Title>{this.props.beast.Title}</Card.Title>
-                    <Card.Text>{this.props.beast.description}</Card.Text>
-                    <Card.Text>💖: {this.state.favorites}</Card.Text>
-                </Card.Body>
-            </Card>
-        
+        <Card style={{ width: "18rem" }}>
+        <Card.Img
+            onClick={this.handleUserClick}
+            variant="top"
+            src={this.props.beast.image_url}
+            alt={this.props.beast.description}
+        />
+        <Card.Body>
+            <Card.Title>{this.props.beast.Title}</Card.Title>
+            <Card.Text>{this.props.beast.description}</Card.Text>
+            <Card.Text>💖: {this.state.favorites}</Card.Text>
+        </Card.Body>
+        </Card>
     </article>
     );
 }
-
 }
 export default HornedBeast;
